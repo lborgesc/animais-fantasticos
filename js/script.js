c28e65704e2f2fa3a8e21fe2fd0487b6a4707f43
Leônidas Borges Caetano <lborgescaetano@gmail.com>
2022-01-31 08:20:14 -0300

    fetch animais refatorado

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ import Tooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
-import initFetchAnimais from './modules/fetch-animais.js';
+import fetchAnimais from './modules/fetch-animais.js';
 import initFetchBitcoin from './modules/fetch-bitcoin.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
 
@@ -27,9 +27,10 @@ tooltip.init();
 
 
 
+fetchAnimais('../../animaisapi.json', '.numeros-grid');
+
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
-initFetchAnimais();
 initFetchBitcoin();
 initAnimacaoScroll();
