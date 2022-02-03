bd80ce7a4df92499f759c72c57fa5ceacdab44e8
Leônidas Borges Caetano <lborgescaetano@gmail.com>
2022-02-02 22:25:12 -0300

    menu mobile refatorado

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ import TabNav from './modules/tabnav.js';
 import Modal from './modules/modal.js';
 import Tooltip from './modules/tooltip.js';
 import DropdownMenu from './modules/dropdown-menu.js';
-import initMenuMobile from './modules/menu-mobile.js';
+import MenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import fetchAnimais from './modules/fetch-animais.js';
 import fetchBitcoin from './modules/fetch-bitcoin.js';
@@ -28,12 +28,14 @@ tooltip.init();
 const animaScroll = new ScrollAnima('[data-anime="scroll"]');
 animaScroll.init();
 
-const dropdownMenu = new DropdownMenu('[data-dropdown]',);
+const dropdownMenu = new DropdownMenu('[data-dropdown]');
 dropdownMenu.init();
+
+const menuMobile = new MenuMobile('[data-menu="button"]','[data-menu="list"]');
 
 fetchAnimais('../../animaisapi.json', '.numeros-grid');
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
 
-initDropdownMenu();
-initMenuMobile();
+
+
 initFuncionamento();
